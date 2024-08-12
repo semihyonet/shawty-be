dc4404452c8d02e178762c3140a5e42833fa5b16
Semih Yonet <semih@shoppingminds.com>
2024-08-13 01:26:41 +0200

    Updated the redis client to use environment variables

--- app/core/cache/redis.py
+++ app/core/cache/redis.py
@@ -2,10 +2,13 @@ from functools import lru_cache
 
 from redis import Redis
 
+from app.core.settings import get_settings
+
 
 @lru_cache
 def get_redis_client() -> Redis:
-    return Redis(host='localhost', port=6379)
+    settings = get_settings()
+    return Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 
 
 redis_client = get_redis_client()
